ee40558460aaea4584039ac15b10aa87e1129b0d
sehako <dhtpgkr1999@gmail.com>
2025-11-22 11:40:31 +0900

    feat(route): 페이지 라우팅 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 import { Container } from 'react-bootstrap';
+import { Route, Routes } from 'react-router-dom'; // 1. Routes 추가
 import BoardDetail from './page/BoardDetail';
+import BoardList from './page/BoardList';
+import BoardWrite from './page/BoardWrite';
 import Nav from './static/Nav';
 
 function App() {
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
-      <Nav></Nav>
+    <div style={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
+      <Nav />
       <Container
         className="position-relative mt-5"
-        style={{ height: '100%', overflowY: 'auto' }}
+        style={{ flex: 1, overflowY: 'auto' }}
       >
-        <BoardDetail></BoardDetail>
+        <Routes>
+          <Route path="/" element={<BoardList />} />
+          <Route path="/post/:no" element={<BoardDetail />} />
+          <Route path="/post/write" element={<BoardWrite />} />
+        </Routes>
       </Container>
     </div>
   );
