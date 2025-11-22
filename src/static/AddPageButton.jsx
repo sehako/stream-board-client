7d5977d4ebb623c99fea8538ffefd9de38db4425
sehako <dhtpgkr1999@gmail.com>
2025-11-22 11:48:07 +0900

    feat(route): 게시글 추가 버튼 클릭 시 게시글 작성 페이지로 이동 기능 구현

--- src/static/AddPageButton.jsx
+++ src/static/AddPageButton.jsx
@@ -1,12 +1,20 @@
 import { Button, Col, Row } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
 function AddPageButton() {
+  const navigate = useNavigate();
+
+  const navigateWritePage = () => {
+    navigate('/post/write');
+  };
+
   return (
     <Row>
       <Col className="position-absolute bottom-0 end-0 ms-3 mt-3 mb-3 me-1 text-end">
         <Button
           variant="dark"
           className="align-items-center justify-content-center rounded-4 text-center bg-dark"
+          onClick={navigateWritePage}
         >
           <span className="fs-1 fw-bold lh-1">+</span>
         </Button>
