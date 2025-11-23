dfeed0238b6d81d2daacdd8b6e315c9ad2895eff
sehako <dhtpgkr1999@gmail.com>
2025-11-23 16:05:44 +0900

    refactor(nav): 네비바  UI 링크 밑줄 제거

--- src/static/Nav.jsx
+++ src/static/Nav.jsx
@@ -6,7 +6,7 @@ function Nav() {
   return (
     <Navbar fixed="top" bg="dark" variant="dark" expand="lg">
       <Container>
-        <Link to={'/'}>
+        <Link to={'/'} className="link-underline link-underline-opacity-0">
           <Navbar.Brand>StreamBoard</Navbar.Brand>
         </Link>
       </Container>
